d05e65fe529d444af758d2333b148855e07c3103
ojogal <ojog.al@gmail.com>
2023-01-10 12:22:47 +0200

    Seeds added

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ApplicationRecord
-  validates :title, presence :true, length: { minimum: 3, maximum: 20 }
-  validates :body, presence :true, length: { maximum: 200 }
+  validates :title, presence: :true, length: { minimum: 3, maximum: 20 }
+  validates :body, presence: :true, length: { maximum: 200 }
 end
