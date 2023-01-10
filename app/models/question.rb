ae3f51644607222f59bb932c4db54ea00dd2094a
ojogal <ojog.al@gmail.com>
2023-01-10 12:41:20 +0200

    Format for date and time method added to Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,8 @@
 class Question < ApplicationRecord
   validates :title, presence: :true, length: { minimum: 3, maximum: 20 }
   validates :body, presence: :true, length: { maximum: 200 }
+
+  def format_time
+    self.created_at.strftime('%Y-%m-%d %H:%M:%S')
+  end
 end
