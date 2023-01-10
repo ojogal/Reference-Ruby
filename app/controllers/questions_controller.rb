6933e99f451388dc5e345eebea81b9403f494584
ojogal <ojog.al@gmail.com>
2023-01-10 11:51:59 +0200

    Questions controller updated

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,25 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[ show update destroy ]
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def question_params
+  end
+
+  def set_question
+  end
 end
